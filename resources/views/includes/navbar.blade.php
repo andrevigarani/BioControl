f7d90375b1bf872840c2f8ee332fb0c4e1cc2e05
André Felipe Vigarani <avlipe44@gmail.com>
2023-09-24 23:09:39 -0300

    Add public views diretory

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">Nome do Seu Site</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Página 1</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Página 2</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Página 3</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Página 4</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Página 5</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Página 6</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Página 7</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
