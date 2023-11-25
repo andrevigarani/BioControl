badc99db2eb8b4eeb736eac68a0000da6ef5a470
André Felipe Vigarani <avlipe44@gmail.com>
2023-11-25 19:38:13 -0300

    general updates

--- resources/views/private/contato/index.blade.php
+++ resources/views/private/contato/index.blade.php
@@ -3,54 +3,55 @@
 @section('content')
 
     <div class="private-header">
-        CONTATOS
+        <h3>CONTATOS</h3>
+
+        <input type="text" class="form-text search-bar">
+        <button type="button" class="btn btn-info">Pesquisar</button>
+
+        <div style="float: right">
+            <button type="button" class="btn btn-success">Novo Contato</button>
+        </div>
+
     </div>
 
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
+    <div class="private-content">
+            @if($contatos->isEmpty())
 
-                @if($contatos->isEmpty())
-                    <p>Não há contatos disponíveis.</p>
+                <table class="table table-striped w-100">
+                    <tr>
+                        <td class="text-center">
+                            Não há contatos cadastrados!
+                        </td>
+                    </tr>
+                </table>
 
-                @else
-                    <table class="table table-striped">
-                        <thead class="text-center">
-                        <th scope="col">NOME</th>
-                        <th scope="col">TELEFONE</th>
-                        <th scope="col">PLA</th>
-                        <th scope="col">Valor</th>
-                        </thead>
-                        <tbody>
-                        @php $total = 0 @endphp
-                        @foreach($bagItems as $bagItem)
-                            <tr style="vertical-align: middle" class="text-center">
-                                <td style="width: 300px">
-                                    <img src="data:image/png;base64, {{ $bagItem->product_image }}"
-                                         alt="{{ $bagItem->product_name }}" style="width: 100%;">
-                                </td>
-                                <td>{{ $bagItem->product_name }}</td>
-                                <td>
-                                    <span onclick="updateQuantity({{$bagItem->product_id}}, 'S')"
-                                          style="margin-right: 20px; cursor: pointer;"><</span>
-                                    <span id="quantityProduct{{$bagItem->product_id}}">{{ $bagItem->quantity }}</span>
-                                    <span onclick="updateQuantity({{$bagItem->product_id}}, 'A')"
-                                          style="margin-left: 20px; cursor: pointer;">></span>
-                                </td>
-                                <td>R$ {{ number_format($bagItem->sub_total,2) }}</td>
-                                @php $total += $bagItem->sub_total @endphp
-                            </tr>
-                        @endforeach
-                        </tbody>
-                        <tfoot>
-                        <tr class="bg-secondary bg-opacity-50">
-                            <td colspan="3">Total</td>
-                            <td class="text-center">R$ {{ number_format($total,2) }}</td>
+            @else
+                <table class="table table-striped w-100">
+                    <thead class="text-center">
+                    <th scope="col">NOME</th>
+                    <th scope="col">TELEFONE</th>
+                    <th scope="col">PLA</th>
+                    <th scope="col">Valor</th>
+                    </thead>
+                    <tbody>
+                    @php $total = 0 @endphp
+                    @foreach($bagItems as $bagItem)
+                        <tr style="vertical-align: middle" class="text-center">
+                            <td>{{ $bagItem->product_name }}</td>
+                            <td>{{ $bagItem->quantity }}</td>
+                            <td>R$ {{ number_format($bagItem->sub_total,2) }}</td>
+                            @php $total += $bagItem->sub_total @endphp
                         </tr>
-                        </tfoot>
-                    </table>
-                @endif
-            </div>
+                    @endforeach
+                    </tbody>
+                    <tfoot>
+                    <tr class="bg-secondary bg-opacity-50">
+                        <td colspan="3">Total</td>
+                        <td class="text-center">R$ {{ number_format($total,2) }}</td>
+                    </tr>
+                    </tfoot>
+                </table>
+            @endif
         </div>
     </div>
 @endsection
